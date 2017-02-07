27be84fa3c5e00a31f17fda7d6b3047fcc3194b4
Bruno Souza <Bruno Souza>
2017-02-07 19:38:01 -0200

    inicio: criando componentes com ng generate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { QuestionsComponent } from './questions/questions.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
