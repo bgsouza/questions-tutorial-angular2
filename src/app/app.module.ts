7238be0aca008e00d11a4e8fcbc85afde1403f3e
Bruno Souza <Bruno Souza>
2017-02-08 19:58:05 -0200

    criando shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,14 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { QuestionsComponent } from './questions/questions.component';
+import { QuestionFormComponent } from './questions/question-form/question-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionsComponent
+    QuestionsComponent,
+    QuestionsComponent,
+    QuestionFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/questions/shared/questions.ts
@@ -0,0 +1,5 @@
+export class Question {
+  id: number;
+  title: string;
+  text: string;
+}
